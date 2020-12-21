7ba359c32a1cd175cd02319bfb61b3b62467d685
Pepous <cyril.guirguis.06@gmail.com>
2020-12-21 22:44:36 +0100

    -Bouton démarrage fonctionnel

--- src/start/Root.java
+++ src/start/Root.java
@@ -5,6 +5,8 @@ import java.awt.FlowLayout;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
@@ -19,8 +21,20 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Root extends JFrame {
-	public Root() throws IOException {
+import engine.Game;
+import engine.GameEngineGraphical;
+import engine.GraphicalInterface;
+import model.Labyrinthe;
+import model.MainPainter;
+import model.Monster;
+import model.Pacman;
+import model.PacmanController;
+import model.PacmanGame;
+
+public class Root extends JFrame implements ActionListener{
+	private boolean gameStarted;
+	public Root() throws IOException, InterruptedException {
+		this.gameStarted=false;
 	    this.setTitle("CovidedButSurvived");
 	    this.setSize(320,320);
 	    this.setLocationRelativeTo(null);
@@ -31,14 +45,16 @@ public class Root extends JFrame {
 		JPanel panneau = new JPanel();
 		
 		BufferedImage logo = ImageIO.read(new File("img/logo.png"));
-		BufferedImage resizedLogo=this.scale(logo, 0.2);
+		BufferedImage resizedLogo=Root.scale(logo, 0.2);
 		JLabel picLabel = new JLabel(new ImageIcon(resizedLogo));
 		panneau.add(picLabel);
 		
 		JButton StartButton = new JButton("Start");
 		panneau.add(StartButton);
+		StartButton.addActionListener(this);
 		this.getContentPane().add(panneau);
 	    this.setVisible(true);
+	    this.run();
 	    
 
 	}
@@ -53,5 +69,16 @@ public class Root extends JFrame {
 	        return op.filter(bi, biNew); 
 	  
 	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		this.gameStarted=true;
+		this.dispose();
+	}
+	public void run() throws InterruptedException {
+		while(gameStarted==false) {
+			Thread.sleep(20);
+		}
+	}
+
 	
 }
